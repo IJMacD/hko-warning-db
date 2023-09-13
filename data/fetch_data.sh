7c85293badaf2765131523622a79ea1601f5c228
Iain MacDonald <IJMacD@gmail.com>
2023-09-13 15:29:28 +0800

    Updated csvdb can create tables directly

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -2,10 +2,12 @@
 
 TMP_DB="tmp-$RANDOM.csv"
 
+csvdb "CREATE TABLE \"$TMP_DB\" (code,start,end)";
+
 (
     echo "id,category,name,type,direction,startTime,startDay,startMonth,startYear,startSorx,endTime,endDay,endMonth,endYear,endSorx,duration";
     curl -s "https://www.hko.gov.hk/dps/wxinfo/climat/warndb/tc.dat" | tr "\t" "," | grep -v UUUU;
-) | csvdb "CREATE TABLE \"$TMP_DB\" AS SELECT 'TC_'||type AS code, MAKE_DATETIME(startYear,startMonth,startDay,startTime/100,startTime%100) AS start, MAKE_DATETIME(endYear,endMonth,endDay,endTime/100,endTime%100) AS end WHERE type != 'X'";
+) | csvdb "INSERT INTO \"$TMP_DB\" SELECT 'TC_'||type AS code, MAKE_DATETIME(startYear,startMonth,startDay,startTime/100,startTime%100) AS start, MAKE_DATETIME(endYear,endMonth,endDay,endTime/100,endTime%100) AS end WHERE type != 'X'";
 
 (
     echo "id,category,name,type,direction,startTime,startDay,startMonth,startYear,startSorx,endTime,endDay,endMonth,endYear,endSorx,duration";
