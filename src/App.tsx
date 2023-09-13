7db841fdc6be2fd7a8d22ca632ecdcb356866dc2
Iain MacDonald <IJMacD@gmail.com>
2023-09-13 14:13:37 +0800

    Add FlipCard preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { useWarningsDB } from './useWarningsDB';
+import { FlipCard } from './FlipCard';
+import { RowObject } from 'csvdb';
+import cards from './cards.json';
 
-// const FIRST_YEAR = 1950;
-// const CURRENT_YEAR = new Date().getFullYear();
-// const years = Array.from({length:CURRENT_YEAR-FIRST_YEAR+1}).map((_,i) => FIRST_YEAR + i);
 const ONE_HOUR = 60 * 60 * 1000;
 const EIGHT_HOURS = 8 * ONE_HOUR;
 
 function App() {
   const [ now, setNow ] = useState(() => Date.now());
-  // const now = useDebounce(rawNow, 50);
-  // const [ year, setYear ] = useState(CURRENT_YEAR);
   const warningsDB = useWarningsDB();
   const [ isPlaying, setIsPlaying ] = useState(false);
-
-  // useEffect(() => {
-  //   const date = new Date(now);
-  //   date.setFullYear(year);
-  //   setNow(+date);
-  // }, [year, now]);
 
   useEffect(() => {
     if (isPlaying) {
@@ -38,19 +30,87 @@ function App() {
     ] :
     [];
 
+
+  const highestCard = getHighestPriority(inForce);
+
+  const previewFlip = false;
+  const previewAllFlip = false;
+
+  if (previewAllFlip) {
+    const selectedIndex = highestCard ? cards.indexOf(highestCard.code) : 0;
+    const sortedCards = [...cards.slice(selectedIndex + 1), ...cards.slice(0, selectedIndex + 1) ];
+
+    return (
+      <>
+        <Header now={now} setNow={setNow} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+        <div className="FlipCard-Wrap" style={{position:"relative",height:450,overflow:"hidden"}}>
+          <FlipCard image={""} style={{position:"absolute"}} />
+          {
+            sortedCards.map(cardID => <FlipCard key={cardID} animated image={getImageURL({code: cardID })} style={{position:"absolute"}} />)
+          }
+        </div>
+        <WarningIconList inForce={inForce} size={64} />
+      </>
+    );
+  }
+
+  if (previewFlip) {
+    return (
+      <>
+        <Header now={now} setNow={setNow} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+        <FlipCard image={highestCard?getImageURL(highestCard):null} />
+        <WarningIconList inForce={inForce} size={64} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Header now={now} setNow={setNow} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+      <WarningIconList inForce={inForce} />
+    </>
+  );
+}
+
+export default App
+
+type HeaderProps = {
+  now: number;
+  setNow: (now: number) => void;
+  isPlaying: boolean;
+  setIsPlaying: ((isPlaying: boolean|((p: boolean) => boolean)) => void);
+}
+
+type WarningIconListProps = {
+  inForce: RowObject[];
+  size?: number;
+}
+
+function getImageURL(card: RowObject): string {
+  if (!card.code) return "";
+  return `${import.meta.env.BASE_URL}${card.code}.webp`;
+}
+
+function WarningIconList ({ inForce, size = 256 }: WarningIconListProps) {
+  return (
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {inForce.map(w => <li key={w.code} style={{ display: "inline-block" }}><img src={getImageURL(w)} style={{ width: size }} /></li>)}
+    </ul>
+  );
+}
+
+function Header ({ now, setNow, isPlaying, setIsPlaying }: HeaderProps) {
+
   const year = new Date(now).getFullYear();
 
   const startDate = new Date(`${year}-01-01T00:00:00`);
   const endDate = new Date(`${year+1}-01-01T00:00:00`);
 
+  const markerStyle = { flex: 1, borderLeft: "1px solid #666", marginTop: 0 };
+
   return (
     <>
       <p>
-        {/* <select value={year} onChange={e => setYear(+e.target.value)}>
-          {
-            years.map(year => <option key={year}>{year}</option>)
-          }
-        </select> */}
         <input
           type="datetime-local"
           value={new Date(now+EIGHT_HOURS).toISOString().substring(0,19)}
@@ -58,16 +118,6 @@ function App() {
             setNow(e.target.valueAsNumber);
           }}
         />
-        {/* <input
-          type="date"
-          value={new Date(now).toISOString().substring(0,10)}
-          onChange={e => setNow(+(e.target.valueAsDate||0))}
-        />
-        <input
-        type="time"
-          value={new Date(now).toISOString().substring(11,19)}
-          onChange={e => setNow(+(e.target.valueAsDate||0))}
-        /> */}
         <button onClick={() => setIsPlaying(p=>!p)}>{isPlaying?"Pause":"Play"}</button>
       </p>
       <label>
@@ -81,26 +131,60 @@ function App() {
         />
       </label>
       <div style={{display:"flex"}}>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>J</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>F</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>M</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>A</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>M</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>J</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>J</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>A</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>S</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>O</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>N</td>
-        <td style={{flex:1,borderLeft:"1px solid #666"}}>D</td>
+        <p style={markerStyle}>J</p>
+        <p style={markerStyle}>F</p>
+        <p style={markerStyle}>M</p>
+        <p style={markerStyle}>A</p>
+        <p style={markerStyle}>M</p>
+        <p style={markerStyle}>J</p>
+        <p style={markerStyle}>J</p>
+        <p style={markerStyle}>A</p>
+        <p style={markerStyle}>S</p>
+        <p style={markerStyle}>O</p>
+        <p style={markerStyle}>N</p>
+        <p style={markerStyle}>D</p>
       </div>
-      <ul style={{listStyle:"none",padding:0}}>
-        {
-          inForce.map(w => <li key={w.code} style={{display:"inline-block"}}><img src={`${import.meta.env.BASE_URL}${w.code}.webp`} style={{width:256}} /></li>)
-        }
-      </ul>
     </>
-  )
+  );
 }
 
-export default App
+function getHighestPriority (cards: RowObject[]) {
+  let highestPriority = 0;
+  let highestCard: RowObject|null = null;
+
+  for (const card of cards) {
+    const priority = getPriority(card);
+    if (priority && priority > highestPriority) {
+      highestCard = card;
+      highestPriority = priority;
+    }
+  }
+
+  return highestCard;
+}
+
+function getPriority (card: RowObject) {
+  return {
+    TC_1:10,
+    TC_3:30,
+    TC_8NE:80,
+    TC_8NW:80,
+    TC_8SE:80,
+    TC_8SW:80,
+    TC_9:90,
+    TC_10:100,
+    THUNDER:5,
+    RAIN_A:20,
+    RAIN_R:40,
+    RAIN_B:50,
+    HOT:3,
+    COLD:3,
+    FROST:4,
+    FIRE_Y:2,
+    FIRE_R:2,
+    FNT:8,
+    LS:7,
+    MSN:6,
+    TMW:120,
+  }[card.code as string];
+}
